6576f4eb78361a37738cc7a243f1c8d5074ffa5a
Prakhar Singh Rajput <prakharsinghrajput@Prakhars-MacBook-Air.local>
2026-02-10 09:39:20 +0530

    update url

--- app/api/axios_Instance.tsx
+++ app/api/axios_Instance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5200",
+  baseURL: "https://api.fertigoapp.com",
   headers: {
     Accept: "application/json",
   },
